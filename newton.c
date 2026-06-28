e95ddb592aa5d143665e93656a03bf3f5e490f2e
8297621642 <saicharanbrahmadevara@gmail.com>
2026-06-28 15:32:09 +0530

    Update newton.c

--- newton.c
+++ newton.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 #include<math.h>
-double Root_calc(double S){
-    double init_guess=S/2.0;//Initial_guess
-    double epsilon=0.000001;//Convergence threshold: stops iterations when change is negligible
-    double last_x;
+#define db double
+
+db Root_calc(db S){
+    db init_guess=S/2.0;//Initial_guess
+    db epsilon=0.000001;//Convergence threshold: stops iterations when change is negligible
+    db last_x;
     do{
         last_x=init_guess;//stores previous value to check for difference and to find whether it is converging
         init_guess=(init_guess+(S/init_guess))/2.0;
@@ -11,7 +13,7 @@ double Root_calc(double S){
     return init_guess;
 }
 int main(){
-    double S;//the number to find the square root of
+    db S;//the number to find the square root of
     scanf("%lf",&S);
     printf("The Square root of %.2lf is %.6lf\n",S,Root_calc(S));
     return 0;
